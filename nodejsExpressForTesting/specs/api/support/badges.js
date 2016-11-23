5b814ce890f28bb58d4bff1525aea554ba7e1a8f
Shenn299 <sebastien.henneberger@gmail.com>
2016-11-23 15:59:10 +0100

    Change environment variable for URL

--- nodejsExpressForTesting/specs/api/support/badges.js
+++ nodejsExpressForTesting/specs/api/support/badges.js
@@ -1,5 +1,5 @@
 // If no API URL address is specified at run time, then system takes the API URL specified in env.json file under "default"
-var apiURL = require('../../../env.json')[process.env.API_URL || 'default'];
+var apiURL = process.env.API_URL || require('../../../env.json').default.API_URL;
 var api = require("supertest-as-promised")(apiURL);
 var Chance = require("chance");
 var chance = new Chance();
